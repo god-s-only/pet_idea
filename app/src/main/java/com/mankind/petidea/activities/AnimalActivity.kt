842d8a1b1b300d53343125194dde4bdb8144e106
joshua <joshxchris098@gmail.com>
2024-12-03 01:43:23 +1200

    iuwudywu

---
+++ app/src/main/java/com/mankind/petidea/activities/AnimalActivity.kt
@@ -0,0 +1,21 @@
+package com.mankind.petidea.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mankind.petidea.R
+
+class AnimalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_animal)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
